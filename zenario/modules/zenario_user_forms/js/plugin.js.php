0788683ee26bf9a38246cf8436a363bf14c488f8
Chris Turnbull <Chris-Turnbull@users.noreply.github.com>
2016-04-07 13:24:25 +0100

    Attempting to sort out a glitched file in the repo

---
+++ zenario/modules/zenario_user_forms/js/plugin.js.php
@@ -0,0 +1,11 @@
+<?php
+
+require CMS_ROOT . 'zenario/libraries/mit/jquery/jquery.fileupload.js';
+echo "\n/**/\n";
+require CMS_ROOT . 'zenario/libraries/mit/jquery/jquery.fileupload-process.min.js';
+echo "\n/**/\n";
+require CMS_ROOT . 'zenario/libraries/mit/jquery/jquery.fileupload-validate.min.js';
+echo "\n/**/\n";
+require CMS_ROOT . 'zenario/libraries/mit/jquery/jquery-ui.progressbar.min.js';
+echo "\n/**/\n";
+require 'plugin.min.js';
