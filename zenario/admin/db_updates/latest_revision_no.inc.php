a8e3d38d134780f5a14bb8e6923128c2ba0d0b5b
Chris Turnbull <Chris-Turnbull@users.noreply.github.com>
2017-10-06 10:54:13 +0100

    Syncing our private svn repo with the public repo on GitHub:
    - Our last patch release caused a bug for anyone using an older version of
      Chrome. We've added a temporary fix that allows them to continue working
      without needing to update their browser to the latest version.
    - Fixed a security vulnerability where an administrator could launch a SQL
      injection attack by renaming a document, generating a public link for the
      document, and then uploading a replacement.
    - Fixed a security vulnerability in older versions of Zenario, where an
      administrator could launch a SQL injection attack by renaming a document.
      (This doesn't affect more recent versions.)

--- zenario/admin/db_updates/latest_revision_no.inc.php
+++ zenario/admin/db_updates/latest_revision_no.inc.php
@@ -27,7 +27,7 @@
  */
 if (!defined('NOT_ACCESSED_DIRECTLY')) exit('This file may not be directly accessed');
 
-define('ZENARIO_REVISION', '42085');
+define('ZENARIO_REVISION', '42990');
 define('LATEST_REVISION_NO', 36452);
 define('LATEST_BIG_CHANGE_REVISION_NO', 36450);
 define('INSTALLER_REVISION_NO', 33430);
@@ -35,8 +35,8 @@ define('INSTALLER_DEFAULT_THEME', 'duke_street');
 
 define('ZENARIO_MAJOR_VERSION', '7');
 define('ZENARIO_MINOR_VERSION', '3');
-define('ZENARIO_RELEASE_VERSION', '5');
-define('ZENARIO_VERSION', '7.3.5');
+define('ZENARIO_RELEASE_VERSION', '6');
+define('ZENARIO_VERSION', '7.3.6');
 define('ZENARIO_IS_HEAD', false);
 
 define('TINYMCE_DIR', 'zenario/libraries/lgpl/tinymce_4_2_6/');
