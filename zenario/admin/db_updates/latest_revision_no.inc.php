852b3ea838cacbc10e31061c7e892b9c51a50428
Chris Turnbull <Chris-Turnbull@users.noreply.github.com>
2018-07-03 10:00:13 +0100

    Syncing our private svn repo with the public repo on GitHub:
    - Fix for a security vulnerability, where an attacker could change the images and/or
      text of of the slides in a "Slideshow 2" plugin.

--- zenario/admin/db_updates/latest_revision_no.inc.php
+++ zenario/admin/db_updates/latest_revision_no.inc.php
@@ -34,5 +34,5 @@ define('INSTALLER_DEFAULT_THEME', 'duke_street');
 
 define('ZENARIO_MAJOR_VERSION', '7');
 define('ZENARIO_MINOR_VERSION', '1');
-define('ZENARIO_RELEASE_VERSION', '6');
-define('ZENARIO_VERSION', '7.1.6');
+define('ZENARIO_RELEASE_VERSION', '7');
+define('ZENARIO_VERSION', '7.1.7');
